bfa8c5aae339e0464796897e751823fa07e41f82
Shubhanshu9696 <92735584+Shubhanshu9696@users.noreply.github.com>
2024-05-27 15:04:11 +0530

    Update data.jsx

--- src/data.jsx
+++ src/data.jsx
@@ -2,9 +2,9 @@ const reviews = [
     {
         id: 1,
         name: "Shubhanshu Tiwari",
-        job: "Project Manager",
+        job: "Frontend Developer",
         image: "shubhanshu.jpg",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo rem nulla fuga maxime reiciendis minima error, reprehenderit facere adipisci culpa in est, eius atque maiores. Dicta ut hic illo assumenda."
+        text: "Shubhanshu Tiwari significantly enhanced our project with exceptional frontend development skills. Their keen eye for design, user-friendly approach, and proficiency in modern frameworks were invaluable. Professional, creative, and communicative, they ensured a successful, timely launch. Their dedication and expertise make them a top recommendation for any frontend project."
     },
     // {
     //     id: 2,
@@ -16,22 +16,22 @@ const reviews = [
     {
         id: 3,
         name: "Shubham Sharma",
-        job: "Software Developer",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo rem nulla fuga maxime reiciendis minima error, reprehenderit facere adipisci culpa in est, eius atque maiores. Dicta ut hic illo assumenda.",
+        job: "Backend Developer",
+        text: "Shubham Kumar transformed our backend infrastructure with outstanding expertise. Their proficiency in database management, server-side logic, and API development was crucial. Reliable, innovative, and detail-oriented, they streamlined our processes and enhanced performance. Their dedication and technical skill make them an invaluable asset. Highly recommended for any backend development needs.",
         image: "shubham.jpg"
     },
     {
         id: 4,
         name: "Vikas Gupta",
-        job: "Web Developer",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo rem nulla fuga maxime reiciendis minima error, reprehenderit facere adipisci culpa in est, eius atque maiores. Dicta ut hic illo assumenda.",
+        job: "Cloud Developer",
+        text: "Vikas Gupta revolutionized our cloud infrastructure with their expertise. Their skills in AWS, Azure, and cloud architecture optimized our performance and scalability. Reliable, innovative, and detail-oriented, they ensured seamless integration and robust security. Their dedication and technical prowess make them an indispensable asset. Highly recommended for any cloud development projects and helped in documentation.",
         image: "vikas.jpg"
     },
     {
         id: 5,
         name: "Sharad Yadav",
-        job: "Frontend Developer",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo rem nulla fuga maxime reiciendis minima error, reprehenderit facere adipisci culpa in est, eius atque maiores. Dicta ut hic illo assumenda.",
+        job: "Documentation",
+        text: "Sharad Yadav meticulously documented our project, ensuring clarity and comprehensiveness. Their attention to detail and organizational skills made complex information easily accessible. Reliable and thorough, they produced high-quality documentation that facilitated smooth communication and efficient workflow. Their dedication and expertise are invaluable. Highly recommended for any documentation needs.",
         image: "sharad.jpg"
     },
     // {
